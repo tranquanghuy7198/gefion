b69896c949cf97c29c3c78762a9a2d620a1ef52f
huy <huy@gamejam.com>
2024-03-15 16:36:09 +0700

    fix: Return all vaults of a owner

--- contracts/core/GefionFactory.sol
+++ contracts/core/GefionFactory.sol
@@ -10,11 +10,18 @@ contract GefionFactory is IGefionFactory, Ownable {
     address public router;
     mapping(address => mapping(address => address)) public override getVault;
     address[] private _allVaults;
+    mapping(address => address[]) _vaultsOf;
 
     constructor() Ownable() {}
 
     function allVaults() external view returns (address[] memory) {
         return _allVaults;
+    }
+
+    function getVaultsOf(
+        address vaultOwner
+    ) external view returns (address[] memory) {
+        return _vaultsOf[vaultOwner];
     }
 
     function setRouter(address router_) external onlyOwner {
@@ -41,6 +48,7 @@ contract GefionFactory is IGefionFactory, Ownable {
             router
         );
         _allVaults.push(address(vault));
+        _vaultsOf[msg.sender].push(address(vault));
         getVault[msg.sender][currency] = address(vault);
         emit VaultCreated(address(vault), msg.sender, currency);
         return address(vault);
