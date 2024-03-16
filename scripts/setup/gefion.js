1820b91f232adda9d84261a88bf6c8819b8b0d51
huy <huy@gamejam.com>
2024-03-16 11:23:13 +0700

    fix Set DEX routers

--- scripts/setup/gefion.js
+++ scripts/setup/gefion.js
@@ -3,6 +3,7 @@ const deployInfo = require("../../deploy.json");
 
 const ROUTER = "contracts/core/GefionRouter.sol:GefionRouter";
 const FACTORY = "contracts/core/GefionFactory.sol:GefionFactory";
+const DEX_ROUTERS = ["0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45"]; // Polygon Mumbai DEX Routers
 
 const setup = async () => {
   // Info
@@ -21,6 +22,13 @@ const setup = async () => {
     .connect(deployer)
     .attach(factoryContract.address)
     .setRouter(deployInfo[networkName][ROUTER]);
+
+  // Set DEX router addresses
+  console.log("Setting DEX router addresses...");
+  await factory
+    .connect(deployer)
+    .attach(factoryContract.address)
+    .addDexRouters(DEX_ROUTERS);
 };
 
 setup();
